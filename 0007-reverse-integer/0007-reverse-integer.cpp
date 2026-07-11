78774cfcffc9deca056048d2f0ff9faeab279955
Gaurav shah <gauravshahh78@gmail.com>
2026-07-11 10:11:18 +0530

    Time: 0 ms (100%), Space: 8.6 MB (54.59%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev = 0;
+        while (x != 0) {
+            int lastterm = x % 10;
+            rev = (rev * 10) + lastterm;
+            x /= 10;
+        }
+        if (rev < INT_MIN || rev > INT_MAX)
+            return 0;
+        return (int)rev;
+    }
+};
