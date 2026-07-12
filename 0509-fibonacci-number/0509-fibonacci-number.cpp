b09f97a7c034368e8bebfe3bc544582a9ea14462
Gaurav shah <gauravshahh78@gmail.com>
2026-07-12 10:00:16 +0530

    Time: 0 ms (100%), Space: 7.9 MB (20.57%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        int t1 = 0, t2 = 1, nextTerm = 0;
+        for (int i = 2; i <= n; i++) {
+            nextTerm = t1 + t2;
+            t1 = t2;
+            t2 = nextTerm;
+        }
+        return nextTerm;
+    }
+};
