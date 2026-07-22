4559e7c56a4f923b0174cc0f2e6a75307f747eff
Gaurav shah <gauravshahh78@gmail.com>
2026-07-22 09:14:18 +0530

    Time: 0 ms (100%), Space: 31.4 MB (46.8%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -7,8 +7,10 @@ public:
             int mid = low + (high -low) / 2;
             if (nums[mid] == target)
                 return mid;
-            else if (target > nums[mid]) low = mid + 1;
-            else high = mid -1;
+            else if (nums[mid] < target) 
+                     low = mid + 1;
+            else 
+            high = mid -1;
         }
         return -1;
     }
